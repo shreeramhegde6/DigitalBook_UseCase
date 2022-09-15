9143cbc18fff8063ef59a4b518027da6ebf0f664
unknown <cogdotnet1708@iiht.tech>
2022-09-15 13:35:17 +0530

    searchBook

--- DigitalBookProject/src/app/app-routing.module.ts
+++ DigitalBookProject/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatbookComponent } from './author/creatbook/creatbook.component';
 import { LoginComponent } from './header/login/login.component';
 import { SignupComponent } from './header/signup/signup.component';
 
 const routes: Routes = [
-  {path:'',component : LoginComponent},
+  {path:'',component : CreatbookComponent},
   {path : 'signup', component : SignupComponent},
    {path:'home',component : LoginComponent},
    
